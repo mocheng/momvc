ecc451ed9f78ea01e8734d2efd9e2d3477bfeaf4
Morgan 2 <morgan.chengmo@gmail.com>
2011-06-14 20:09:18 +0800

    action context can be ended with string or object

--- test/testAction.js
+++ test/testAction.js
@@ -1,5 +1,7 @@
 var action = require('../lib/action'),
+    template = require('../lib/template'),
     events = require('events'),
+    mustache = require('mustache'),
     nodeunit = require('nodeunit');
 
 exports['test action'] = nodeunit.testCase({
@@ -12,6 +14,7 @@ exports['test action'] = nodeunit.testCase({
     },
 
     'test action complete immediately' : function(test) {
+        test.expect(2);
         var testAction = function(ctx) {
             ctx.end();
             test.equal(ctx.isDone(), true, 'the context shold have been done');
@@ -20,12 +23,16 @@ exports['test action'] = nodeunit.testCase({
         var req = new events.EventEmitter(),
             res = new events.EventEmitter();
 
+        res.end = function(result) {
+            test.equal(result, undefined, 'should be undefined');
+        };
+
         action.processAction(req, res, testAction);
         test.done();
     },
 
     'test action completed asynchronously' : function(test) {
-        test.expect(2);
+        test.expect(3);
 
         var testAction = function(ctx) {
             setTimeout(function() {
@@ -40,7 +47,52 @@ exports['test action'] = nodeunit.testCase({
         var req = new events.EventEmitter(),
             res = new events.EventEmitter();
 
+        res.end = function(result) {
+            test.equal(result, undefined, 'should be undefined');
+        };
+
         action.processAction(req, res, testAction);
+    },
+
+    'test action completed with string' : function(test) {
+        var testAction = function(ctx) {
+            ctx.end('hello world');
+            test.equal(ctx.isDone(), true, 'the context shold have been done');
+        };
+
+        var req = new events.EventEmitter(),
+            res = new events.EventEmitter();
+
+        res.end = function (result) {
+            test.equal('hello world', result);
+            test.done();
+        }
+
+        action.processAction(req, res, testAction);
+    },
+
+    'test action completed with object' : function(test) {
+        var testAction = function(ctx) {
+            ctx.end({foo: 'bar'});
+            test.equal(ctx.isDone(), true, 'the context shold have been done');
+        };
+
+        var req = new events.EventEmitter(),
+            res = new events.EventEmitter();
+            _render = template.render;
+
+        template.render = function(result) {
+            return mustache.to_html('hello world, {{foo}}', result);
+        };
+        res.end = function (result) {
+            test.equal('hello world, bar', result);
+            test.done();
+        }
+
+        action.processAction(req, res, testAction);
+
+        template.render = _render;
     }
+
 });
 
