a3aa27526cf6e595ea6de63795934aa5ecbc6fa0
Morgan 2 <morgan.chengmo@gmail.com>
2011-06-16 17:36:35 +0800

    add dispatcher and ut

---
+++ test/testDispatcher.js
@@ -0,0 +1,42 @@
+var Dispatcher = require('../lib/dispatcher'),
+    route = require('../lib/route'),
+    nodeunit = require('nodeunit');
+
+exports['test Dispatcher'] = nodeunit.testCase({
+    setUp: function(cb) {
+        cb();
+    },
+
+    tearDown: function(cb) {
+        cb();
+    },
+
+    'test dispatch' : function(test) {
+        var routeconfig = [
+                {
+                    name: 'root',
+                    path: '/',
+                    params: {
+                        module: 'm',
+                        action: 'a'
+                    }
+                }
+            ],
+            mockRoute = new route.Route(routeconfig),
+            dispatcher = new Dispatcher(mockRoute),
+            req = {url: '/'},
+            res = {},
+
+            mockAction = function(ctx) {
+            };
+
+        dispatcher.getAction = function(routePath) {
+            test.equals(routePath, 'm/a');
+            return mockAction;
+        };
+
+        dispatcher.dispatch(req, res);
+
+        test.done();
+    }
+});
